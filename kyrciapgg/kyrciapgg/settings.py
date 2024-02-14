a75db7e9ec397944b7d6aa73e2718f281749c27b
Patryk2kyrtaP <polo.polo.polo@wp.pl>
2024-02-14 23:12:57 +0100

    workflows

--- kyrciapgg/kyrciapgg/settings.py
+++ kyrciapgg/kyrciapgg/settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/kyrciapp/static/"
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / '/kyrciapp/static/',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
