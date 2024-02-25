b703757bf1e48d3a890f00600bdb2b7ea72a8a9d
Patryk2kyrtaP <polo.polo.polo@wp.pl>
2024-02-25 19:13:24 +0100

    final

--- config.py
+++
@@ -1,9 +0,0 @@
-def get_api_key():
-    return "RGAPI-196b9db9-4f70-40f3-bb3c-4d8a264d9eef"
-
-import cassiopeia as cass
-def setup_cassiopeia():
-    api_key = get_api_key()
-    cass.set_riot_api_key(api_key)
-
-
--- maja.py
+++
@@ -1,6 +0,0 @@
-# zmienna1 = 4
-
-# zamienna2 = 1
-
-# zmienna3 = zmienna1 + zamienna2
-# print(f'Wynik dodawania = {zmienna3}')
